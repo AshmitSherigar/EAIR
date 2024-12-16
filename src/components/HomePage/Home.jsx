db200a28cfbb8adf661d596314483daa391da701
Ashmit Sherigar <sherigarashmit@gmail.com>
2024-12-17 00:18:55 +0530

    Scrolling Robot Updated

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import "./Home.css"
-function Home() {
+import React from 'react';
+import CanvasAnimation from './CanvasAnimation';
+import './Home.css';
+
+const Home = () => {
   return (
-    <div className='Home'>
-        <h2>HomePage</h2>
+    <div className="main" id="main">
+      <div className="wrapper">
+        <div className="page1">
+          <CanvasAnimation />
+        </div>
+        <div className="page2"></div>
+        <div className="page3"></div>
+        <div className="page4"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
